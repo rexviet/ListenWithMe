5ce3623b0a9418220c20d449b020f2dfe205eb38
rexviet <rexviet@gmail.com>
2018-07-02 21:54:42 +0700

    format bash script

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-printf "Updating server"
+printf "Updating server\n"
 pm2 reload ListenWithMe
 pm2 reload SongWorker
 
-printf "\nUpdating client"
+printf "\nUpdating client\n"
 cp -r client/* /var/www/listenwithme/
 
-printf "\nClear cache client"
+printf "\nClear cache client\n"
 curl -X DELETE -H "X-Auth-Email: rexviet@gmail.com" -H "X-Auth-Key: 7c4763ac9c1bf24ed463be0cc6c82f8e97fb9" -H "Content-Type: application/json" -d '{"purge_everything": true}' "https://api.cloudflare.com/client/v4/zones/67800206f5f4c0a63d79b6353883c257/purge_cache"
 
-printf "\nDone."
+printf "\n\nDone."
